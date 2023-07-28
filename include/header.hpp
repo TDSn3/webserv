cdf946a4162db1619ead9c6b38bc4fdf33b7fe33
Thomas Da Silva <thomas-78000@hotmail.fr>
2023-07-28 13:02:32 +0200

    header update

--- include/header.hpp
+++ include/header.hpp
@@ -6,12 +6,30 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 15:58:36 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/07/28 11:13:36 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/07/28 12:08:45 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HEADER_HPP
 # define HEADER_HPP
+
+# include <unistd.h>
+# include <string.h>
+# include <errno.h>
+# include <arpa/inet.h>
+# include <sys/select.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <poll.h>
+// # include <sys/epoll.h>
+# include <sys/event.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
 
 # include <iostream>
 
---
+++ object/main.d
@@ -0,0 +1,5 @@
+object/main.o: source/main.cpp include/header.hpp include/colors.hpp
+
+include/header.hpp:
+
+include/colors.hpp:
